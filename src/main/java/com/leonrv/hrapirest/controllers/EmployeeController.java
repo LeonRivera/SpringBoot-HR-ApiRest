21f83627de59d863c21b1f3e306fc5fc30e4549f
LeonRivera <leonriv2@gmail.com>
2022-07-16 16:09:48 -0500

    16072022 | LeonRivera | Endpoint list of employees

--- src/main/java/com/leonrv/hrapirest/controllers/EmployeeController.java
+++ src/main/java/com/leonrv/hrapirest/controllers/EmployeeController.java
@@ -1,12 +1,34 @@
 package com.leonrv.hrapirest.controllers;
 
 import org.springframework.web.bind.annotation.*;
+
+import com.leonrv.hrapirest.dtos.EmployeeDto;
 import com.leonrv.hrapirest.models.*;
 import com.leonrv.hrapirest.repositories.*;
+import java.util.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.data.domain.*;
+import org.springframework.http.*;
+import com.leonrv.hrapirest.services.*;
 
+
+// public class EmployeeController extends GenericController<Employee, Integer> {
+//     public EmployeeController(IGenericRepository<Employee, Integer> repository) {
+//         super(repository);
+//     }
 @RestController @RequestMapping("/api/v1/employee") @CrossOrigin("*")
-public class EmployeeController extends GenericController<Employee, Integer> {
+public class EmployeeController{
+
+    GenericService<Employee, Integer> service;
+
     public EmployeeController(IGenericRepository<Employee, Integer> repository) {
-        super(repository);
+        this.service = new GenericService<Employee, Integer>(repository) {};
+    }
+
+    // @Override
+    @GetMapping("")
+    public ResponseEntity<List<EmployeeDto>> findAllE(){
+        List<EmployeeDto> employeeDtos = EmployeeDto.employeesToDtos(service.findAll());
+        return ResponseEntity.ok(employeeDtos);
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,5 +10,9 @@ VALUES
 ('2022-07-15', 'Description', 1,'Fixed-Term' ),
 ('2022-07-15', 'Description', 1,'External' );
 
+INSERT INTO api_hr.employee (birth_date, cell_phone, date_created, email, is_active, last_name, name, tax_id_number)
+VALUES
+('2001-09-03', '5525490959', '2022-07-16 15:46:09', 'leonriv2@gmail.com', 1, 'Rivera', 'Leon', 'RIRL010903UC9');
+
 
 
